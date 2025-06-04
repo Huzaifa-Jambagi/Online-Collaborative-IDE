0b4b1994c208a2ea5a4852890e2eafcd4d85d3d8
Huzaifa Jambagi <huzaifajambagi10@gmail.com>
2025-06-04 23:48:46 +0530

    Updated editor component with AI integration and code review

--- frontend/src/Components/Editor.jsx
+++ frontend/src/Components/Editor.jsx
@@ -2,25 +2,52 @@ import { cpp } from '@codemirror/lang-cpp';
 import { java } from '@codemirror/lang-java';
 import { javascript } from '@codemirror/lang-javascript';
 import { python } from '@codemirror/lang-python';
+import { GoogleGenAI } from "@google/genai";
 import { dracula } from '@uiw/codemirror-theme-dracula';
 import CodeMirror from '@uiw/react-codemirror';
 import axios from 'axios';
 import 'codemirror/lib/codemirror.css';
 
+
 import { useState } from 'react';
+import toast from 'react-hot-toast';
 
 const languages = [
-{ name: "C++", id: "cpp", version: "10.2.0", extension: cpp() },
+  { name: "C++", id: "cpp", version: "10.2.0", extension: cpp() },
   { name: "Java", id: "java", version: "15.0.2", extension: java() },
   { name: "Python", id: "python", version: "3.10.0", extension: python() },
   { name: "JavaScript", id: "javascript", version: "18.15.0", extension: javascript() },
- { name: "C", id: "c", version: "10.2.0", extension: cpp() }
+  { name: "C", id: "c", version: "10.2.0", extension: cpp() }
 ];
+
+const key = import.meta.env.VITE_API_KEY
+const ai = new GoogleGenAI({ apiKey: key });
 
 const Editor = () => {
   const [selectedLang, setSelectedLang] = useState(languages[3]);
   const [code, setCode] = useState('');
   const [output, setOutput] = useState('');
+
+  const reviewCode = async () => {
+    try{
+    const response = await ai.models.generateContent({
+      model: "gemini-2.0-flash",
+      contents: `You are an expert software engineer and code reviewer.
+
+ Analyze the following code for:
+1. Syntax errors
+2. Logical bugs
+3. Bad practices
+4. Optimization opportunities
+Suggest improvements and fixes **with corrected code**. If the code is correct, explain why it works and dont make it very long.
+
+Code:${code}`,
+    });
+    setOutput(response.text);
+  }catch(error){
+    toast.error("Something went wrong with the AI")
+  }
+  }
 
   const runCode = async () => {
     try {
@@ -53,7 +80,7 @@ const Editor = () => {
             ))}
           </select>
           <button onClick={runCode} className="btn btn-success">Run</button>
-          <button className="btn btn-secondary ms-2">Review</button>
+          <button onClick={reviewCode} className="btn btn-secondary ms-2">Review</button>
         </div>
 
         <div className="flex-grow-1 position-relative">
